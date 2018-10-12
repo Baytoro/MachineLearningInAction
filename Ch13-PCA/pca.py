37ac121b3aad514708f3289f7a92a7a7d167d5c4
Hongjiang Chen <hjchen0725@hotmail.com>
2018-10-13 00:04:36 +0800

    PCA fixed

--- Ch13-PCA/pca.py
+++ Ch13-PCA/pca.py
@@ -4,29 +4,58 @@ Created on Jun 1, 2011
 @author: Peter Harrington
 '''
 from numpy import *
+import matplotlib.pyplot as plt
+
 
 def loadDataSet(fileName, delim='\t'):
     fr = open(fileName)
     stringArr = [line.strip().split(delim) for line in fr.readlines()]
-    datArr = [map(float,line) for line in stringArr]
+    # print(stringArr[0])
+    datArr = [list(map(float, line)) for line in stringArr]
+    # print(datArr[0])
     return mat(datArr)
+
 
 def pca(dataMat, topNfeat=9999999):
     meanVals = mean(dataMat, axis=0)
-    meanRemoved = dataMat - meanVals #remove mean
-    covMat = cov(meanRemoved, rowvar=0)
-    eigVals,eigVects = linalg.eig(mat(covMat))
-    eigValInd = argsort(eigVals)            #sort, sort goes smallest to largest
-    eigValInd = eigValInd[:-(topNfeat+1):-1]  #cut off unwanted dimensions
-    redEigVects = eigVects[:,eigValInd]       #reorganize eig vects largest to smallest
-    lowDDataMat = meanRemoved * redEigVects#transform data into new dimensions
+    meanRemoved = dataMat - meanVals  # remove mean
+    covMat = cov(meanRemoved, rowvar=False)
+    eigVals, eigVects = linalg.eig(mat(covMat))
+    # print(type(eigVects))
+    eigValInd = argsort(eigVals)  # sort, sort goes smallest to largest
+    # print(eigValInd)
+    eigValInd = eigValInd[-1:-(topNfeat + 1):-1]  # cut off unwanted dimensions
+    # reorganize eig vects largest to smallest
+    # print(type(eigVects))
+    # print(type(eigValInd))
+    redEigVects = eigVects[:, eigValInd]
+    lowDDataMat = meanRemoved * redEigVects  # transform data into new dimensions
     reconMat = (lowDDataMat * redEigVects.T) + meanVals
     return lowDDataMat, reconMat
 
-def replaceNanWithMean(): 
+
+def replaceNanWithMean():
     datMat = loadDataSet('secom.data', ' ')
     numFeat = shape(datMat)[1]
     for i in range(numFeat):
-        meanVal = mean(datMat[nonzero(~isnan(datMat[:,i].A))[0],i]) #values that are not NaN (a number)
-        datMat[nonzero(isnan(datMat[:,i].A))[0],i] = meanVal  #set NaN values to mean
+        # values that are not NaN (a number)
+        meanVal = mean(datMat[nonzero(~isnan(datMat[:, i].A))[0], i])
+        # set NaN values to mean
+        datMat[nonzero(isnan(datMat[:, i].A))[0], i] = meanVal
     return datMat
+
+
+if __name__ == '__main__':
+    ld = loadDataSet('testSet.txt')
+    # print(shape(ld))
+    l, r = pca(ld, 1)
+    # print(shape(l))
+    fig = plt.figure(0)
+    ax = fig.add_subplot(111)
+    ax.scatter(ld[:, 0].flatten().A[0],
+               ld[:, 1].flatten().A[0], marker='^', s=60)
+
+    ax.scatter(r[:, 0].flatten().A[0],
+               r[:, 1].flatten().A[0], marker='o', s=50, color='red')
+
+    plt.show()
